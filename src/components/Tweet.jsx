8d601f36d7d992c2a1ac9012c7a30c54e9440455
Ale Mendoza <aletinmendoza@hotmail.com>
2022-10-24 19:58:40 -0600

    Added base layout and some styles

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,11 @@
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
+
+const Tweet = () => {
+    return (
+        <TwitterTweetEmbed
+        tweetId='1582878437101821952'
+        />
+    );
+};
+
+export default Tweet;
